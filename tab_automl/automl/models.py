584e9ae2581bcd3e6ebfcfe40cb55deba35f534d
Tihsrah <harshlf4@gmail.com>
2022-02-11 23:07:19 +0530

    [This PR adds 5 clustering models corrected {#32}]
    Affinity Propagation
    Agglomerative Clustering
    Birch
    DBSCAN
    KMeans
    
    Push.

--- tab_automl/automl/models.py
+++ tab_automl/automl/models.py
@@ -6,6 +6,11 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
 from sklearn.tree import DecisionTreeClassifier
 
+from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import Birch
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 
 single_model_dict = {
     "regression": {
@@ -19,5 +24,12 @@ single_model_dict = {
         "Light Gradient Boosting Classifier": LGBMClassifier,
         "Random Forest Classifier": RandomForestClassifier
     },
+    "clustering":{
+        "Affinity Propagation":AffinityPropagation,
+        "Agglomerative Clustering":AgglomerativeClustering,
+        "Birch":Birch,
+        "DBSCAN":DBSCAN,
+        "KMeans":KMeans
+    }
     
 }
