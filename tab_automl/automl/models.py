84e66d3b1376aef810dfbb53d4f7da7a2037673a
Tihsrah <harshlf4@gmail.com>
2022-02-11 15:13:54 +0530

    [This PR adds clustering models  {#32}]
    Add clustering models inside single_model_dict

--- tab_automl/automl/models.py
+++ tab_automl/automl/models.py
@@ -6,17 +6,30 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
 from sklearn.tree import DecisionTreeClassifier
 
+from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import Birch
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 
 single_model_dict = {
     "regression": {
         "Linear Regression": LinearRegression,
         "Lasso Regression": Lasso,
         "Ridge Regression": Ridge,
-        "Random Forest Regression": RandomForestRegressor
+        "Random Forest Regression": RandomForestRegressor,
     },
     "classification": {
         "Decision Tree Classifier": DecisionTreeClassifier,
         "Light Gradient Boosting Classifier": LGBMClassifier,
         "Random Forest Classifier": RandomForestClassifier
+    },
+    "clustering":{
+        "AffinityPropagation":AffinityPropagation,
+        "AgglomerativeClustering":AgglomerativeClustering,
+        "Birch":Birch,
+        "DBSCAN":DBSCAN,
+        "KMeans":KMeans
+
     }
 }
