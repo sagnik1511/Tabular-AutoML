66e256bcf0c529a338bb3e03cd864cf7da6b0f05
Tihsrah <67807269+Tihsrah@users.noreply.github.com>
2022-02-11 23:05:44 +0530

    Update models.py

--- tab_automl/automl/models.py
+++ tab_automl/automl/models.py
@@ -6,11 +6,6 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
 from sklearn.tree import DecisionTreeClassifier
 
-from sklearn.cluster import AffinityPropagation
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import Birch
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import KMeans
 
 single_model_dict = {
     "regression": {
@@ -24,11 +19,5 @@ single_model_dict = {
         "Light Gradient Boosting Classifier": LGBMClassifier,
         "Random Forest Classifier": RandomForestClassifier
     },
-    "clustering":{
-        "Affinity Propagation":AffinityPropagation,
-        "Agglomerative Clustering":AgglomerativeClustering,
-        "Birch":Birch,
-        "DBSCAN":DBSCAN,
-        "KMeans":KMeans
-    }
+    
 }
